cccc7b3a0e939016cacdae5bbeaa0f3c7224b0fb
Zjunsheng-2343 <719766037@qq.com>
2020-03-30 11:59:25 +0800

    fix

--- member-center/member-provider/src/main/java/member/provider/controller/rabbitMqDelayController.java
+++ member-center/member-provider/src/main/java/member/provider/controller/rabbitMqDelayController.java
@@ -1,26 +1,26 @@
-package member.provider.controller;
-
-import member.center.com.pojo.Order;
-import member.provider.rabbitMq.DelayMsgProducer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-
-@RestController
-public class rabbitMqDelayController {
-
-    @Autowired
-    private DelayMsgProducer delayMsgProducer;
-
-    @RequestMapping("/send")
-    public String sendMsg(){
-        Order order = new Order(1,"0847998","这是延迟10s消息");
-        System.out.println("开始发送的延迟消息时间:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-        delayMsgProducer.delayMsgSend(order,"lazy_exchange","lazy.msg");
-        return "send success!";
-    }
-}
+//package member.provider.controller;
+//
+//import member.center.com.pojo.Order;
+//import member.provider.rabbitMq.DelayMsgProducer;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+//
+//@RestController
+//public class rabbitMqDelayController {
+//
+//    @Autowired
+//    private DelayMsgProducer delayMsgProducer;
+//
+//    @RequestMapping("/send")
+//    public String sendMsg(){
+//        Order order = new Order(1,"0847998","这是延迟10s消息");
+//        System.out.println("开始发送的延迟消息时间:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+//        delayMsgProducer.delayMsgSend(order,"lazy_exchange","lazy.msg");
+//        return "send success!";
+//    }
+//}
--- member-center/member-provider/src/main/java/member/provider/rabbitMq/DelayMsgConsumer.java
+++ member-center/member-provider/src/main/java/member/provider/rabbitMq/DelayMsgConsumer.java
@@ -1,42 +1,42 @@
-package member.provider.rabbitMq;
-
-import com.rabbitmq.client.Channel;
-import member.center.com.pojo.Order;
-import org.springframework.amqp.rabbit.annotation.*;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.handler.annotation.Headers;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Component;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map;
-
-@Component
-public class DelayMsgConsumer {
-
-    /**
-     * 延迟消息消费
-     * @param order
-     * @param changel
-     * @param heads
-     * @throws Exception
-     * @Payload  Order order 用实体接受信息时需要加上注解@Payload  当然可以直接用map来接
-     */
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(value ="lazy_queue",durable = "true"),  //表示队列信息
-            exchange = @Exchange(value = "lazy_exchange",durable = "ture",//表示交换机名称
-                    type = "topic",  //交换机信息 direct，topic，fanout
-                    delayed = "true", //延迟发送
-                    ignoreDeclarationExceptions = "true"),  //支持过滤
-            key = "lazy.*"  //表示路由信息
-    ))
-    public void  getDelayMsg(@Payload Order order, Channel changel, @Headers Map<String,Object> heads) throws Exception{
-        System.out.println("收到的延迟消息的时间:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-        System.out.println("收到的延迟信息是:"+ order.toString());
-        //消息消费ack -- 表示已经消费了
-        Long tag = (Long)heads.get(AmqpHeaders.DELIVERY_TAG);
-        changel.basicAck(tag,false); //false 表示手动ack
-    }
-
-}
+//package member.provider.rabbitMq;
+//
+//import com.rabbitmq.client.Channel;
+//import member.center.com.pojo.Order;
+//import org.springframework.amqp.rabbit.annotation.*;
+//import org.springframework.amqp.support.AmqpHeaders;
+//import org.springframework.messaging.handler.annotation.Headers;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.stereotype.Component;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//import java.util.Map;
+//
+//@Component
+//public class DelayMsgConsumer {
+//
+//    /**
+//     * 延迟消息消费
+//     * @param order
+//     * @param changel
+//     * @param heads
+//     * @throws Exception
+//     * @Payload  Order order 用实体接受信息时需要加上注解@Payload  当然可以直接用map来接
+//     */
+//    @RabbitListener(bindings = @QueueBinding(
+//            value = @Queue(value ="lazy_queue",durable = "true"),  //表示队列信息
+//            exchange = @Exchange(value = "lazy_exchange",durable = "ture",//表示交换机名称
+//                    type = "topic",  //交换机信息 direct，topic，fanout
+//                    delayed = "true", //延迟发送
+//                    ignoreDeclarationExceptions = "true"),  //支持过滤
+//            key = "lazy.*"  //表示路由信息
+//    ))
+//    public void  getDelayMsg(@Payload Order order, Channel changel, @Headers Map<String,Object> heads) throws Exception{
+//        System.out.println("收到的延迟消息的时间:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+//        System.out.println("收到的延迟信息是:"+ order.toString());
+//        //消息消费ack -- 表示已经消费了
+//        Long tag = (Long)heads.get(AmqpHeaders.DELIVERY_TAG);
+//        changel.basicAck(tag,false); //false 表示手动ack
+//    }
+//
+//}
