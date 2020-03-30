58bf6d70c153dd53caa31594bb54d9d0db9b9f07
Zjunsheng-2343 <719766037@qq.com>
2020-03-30 23:07:01 +0800

    fix

--- member-center/member-provider/src/main/java/member/provider/controller/TestController.java
+++ member-center/member-provider/src/main/java/member/provider/controller/TestController.java
@@ -8,7 +8,7 @@ public class TestController {
     @RequestMapping("/docker")
     public String doker(){
 
-        return "Hello docker!2222222222222";
+        return "Hello docker!333333333";
     }
 
 }
