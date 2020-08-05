e9f0f689439f31f3c8432cea99886eb717d3768e
zhongjunsheng <952161135@qq.com>
2020-08-05 13:42:07 +0800

    add:

---
+++ member-center/member-provider/src/main/java/member/provider/common/utils/NetUtils.java
@@ -0,0 +1,15 @@
+package member.provider.common.utils;
+
+
+import cn.hutool.core.util.NetUtil;
+
+/**
+ * 网络相关工具
+ *
+ * @author Administrator
+ * @date 2019/8/19
+ * @since 1.0.0
+ */
+public class NetUtils extends NetUtil {
+
+}
